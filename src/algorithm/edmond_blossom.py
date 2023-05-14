a1394f1056fb8782342ca700defece655018f4a5
Byakko97 <enriquejh97@gmail.com>
2023-05-14 02:54:37 -0300

    encapsule algorithm as a class

--- src/algorithm/edmond_blossom.py
+++ src/algorithm/edmond_blossom.py
@@ -2,121 +2,122 @@
 from collections import deque
 
 from src.common.blossom import Blossom
-from src.common.graph import Graph
 from src.common.union_find import UnionFind
 
-g = Graph()
-g.read()
+class EdmondsBlossom():
 
-def add_child(v, u, e):
-    u.root = v.root
-    u.parent = e.twin
-    u.color = 1 - v.color
-    u.depth = v.depth + 1
+    def __init__(self, g):
+        self.g = g
+        self.expansion_list = []
+        self.dsu = UnionFind(g.vertices)
 
-expansion_list = []
-def switch_match(e):
-    e.switch()
-    if e.matched:
-        # se a aresta entrou no emparelhamento, devemos ajeitar os
-        # extremos dela se eles forem florações
-        f = e
-        for _ in range(2):
-            v = dsu.find(f.to)
-            if isinstance(v, Blossom):
-                expansion_list.append([v, f.to])
-            f = e.twin
-def alternate(v):
-    if v.parent == None:
-        return
-    switch_match(v.parent)
-    alternate(dsu.find(v.parent.to))
-
-def go_up(u, edges, path):
-    path.append(u)
-    edges.append(u.parent)
-    return dsu.find(u.parent.to)
-
-def find_cycle(u, v, u_to_v):
-    path_u = []
-    path_v = []
-    edges_u = []
-    edges_v = []
-    while u.depth > v.depth:
-        u = go_up(u, edges_u, path_u)
-    while v.depth > u.depth:
-        v = go_up(v, edges_v, path_v)
-    while u != v:
-        u = go_up(u, edges_u, path_u)
-        v = go_up(v, edges_v, path_v)
-    cycle = [u] + path_u[::-1] + path_v
-    edge_cycle = [e.twin for e in edges_u][::-1] + [u_to_v] + edges_v
-    return cycle, edge_cycle
-
-dsu = UnionFind(g.vertices)
-
-while True:
-
-    q = deque()
-
-    for v in g.vertices:
-        v.color = -1
-        v.parent = None
-        v.root = v
-        v.depth = 0
-        if not v.matched():
-            # adiciono os vértices não emparelhados na fila
-            done = False
-            v.color = 0
-            q.append(v)
-    
-    augmenting_path = False
-
-    while len(q) > 0:
-        v = q.popleft()
-        x = dsu.find(v)
-        if v != x:
-            # se o vértice foi contraido por outro, já não é válido no novo grafo
+    def run(self):
+        while self.augment():
             continue
-        v = x
-        for e in v.adjacency:
-            u = dsu.find(e.to)
-            if u.color == 0:
-                if u.root != v.root:
-                    # caminho aumentante achado
-                    switch_match(e)
-                    alternate(u)
-                    alternate(v)
-                    augmenting_path = True
-                    break
-                else:
-                    # contrai a floração
-                    cycle, edge_cycle = find_cycle(u, v, e.twin)
-                    blossom = Blossom(dsu, cycle, edge_cycle)
-                    q.append(blossom)
-                    break
-            elif u.color == -1:
-                # extende a árvore alternante
-                add_child(v, u, e)
-                matched_e = u.get_match()
-                add_child(u, matched_e.to, matched_e)
-                q.append(matched_e.to)
+        self.g.print_matching()
 
-        if augmenting_path == True:
-            # expande as florações por onde passou o caminho aumentante
-            for [blossom, expose] in expansion_list:
-                blossom.expand(dsu, expose)
-            expansion_list = []
-            break
+    def augment(self):
+        q = deque()
+        
+        for v in self.g.vertices:
+            v.color = -1
+            v.parent = None
+            v.root = v
+            v.depth = 0
+            if not v.matched():
+                # adiciono os vértices não emparelhados na fila
+                done = False
+                v.color = 0
+                q.append(v)
+        
+        augmenting_path = False
 
-    # expande todas as florações
-    for v in g.vertices:
-        x = dsu.find(v)
-        if isinstance(x, Blossom):
-            x.expand(dsu)
+        while len(q) > 0:
+            v = q.popleft()
+            x = self.dsu.find(v)
+            if v != x:
+                # se o vértice foi contraido por outro, já não é válido no novo grafo
+                continue
+            v = x
+            for e in v.adjacency:
+                u = self.dsu.find(e.to)
+                if u.color == 0:
+                    if u.root != v.root:
+                        # caminho aumentante achado
+                        self.__switch_match(e)
+                        self.__alternate(u)
+                        self.__alternate(v)
+                        augmenting_path = True
+                        break
+                    else:
+                        # contrai a floração
+                        cycle, edge_cycle = self.__find_cycle(u, v, e.twin)
+                        blossom = Blossom(self.dsu, cycle, edge_cycle)
+                        q.append(blossom)
+                        break
+                elif u.color == -1:
+                    # extende a árvore alternante
+                    self.__add_child(v, u, e)
+                    matched_e = u.get_match()
+                    self.__add_child(u, matched_e.to, matched_e)
+                    q.append(matched_e.to)
 
-    if augmenting_path == False:
-        # emparelhamento máximo achado
-        break
+            if augmenting_path == True:
+                # expande as florações por onde passou o caminho aumentante
+                for [blossom, expose] in expansion_list:
+                    blossom.expand(self.dsu, expose)
+                expansion_list = []
+                break
 
-g.print_matching()
+        # expande todas as florações
+        for v in self.g.vertices:
+            x = self.dsu.find(v)
+            if isinstance(x, Blossom):
+                x.expand(self.dsu)
+
+        return augmenting_path
+
+    def __add_child(self, v, u, e):
+        u.root = v.root
+        u.parent = e.twin
+        u.color = 1 - v.color
+        u.depth = v.depth + 1
+
+    def __switch_match(self, e):
+        e.switch()
+        if e.matched:
+            # se a aresta entrou no emparelhamento, devemos ajeitar os
+            # extremos dela se eles forem florações
+            f = e
+            for _ in range(2):
+                v = self.dsu.find(f.to)
+                if isinstance(v, Blossom):
+                    self.expansion_list.append([v, f.to])
+                f = e.twin
+
+    def __alternate(self, v):
+        if v.parent == None:
+            return
+        self.__switch_match(v.parent)
+        self.__alternate(self.dsu.find(v.parent.to))
+
+    def __go_up(self, u, edges, path):
+        path.append(u)
+        edges.append(u.parent)
+        return self.dsu.find(u.parent.to)
+
+    def __find_cycle(self, u, v, u_to_v):
+        path_u = []
+        path_v = []
+        edges_u = []
+        edges_v = []
+        while u.depth > v.depth:
+            u = self.__go_up(u, edges_u, path_u)
+        while v.depth > u.depth:
+            v = self.__go_up(v, edges_v, path_v)
+        while u != v:
+            u = self.__go_up(u, edges_u, path_u)
+            v = self.__go_up(v, edges_v, path_v)
+        cycle = [u] + path_u[::-1] + path_v
+        edge_cycle = [e.twin for e in edges_u][::-1] + [u_to_v] + edges_v
+        return cycle, edge_cycle
