cc185a4ba26ce8d8ccec5a686f788b8f73ab7bbe
Byakko97 <enriquejh97@gmail.com>
2023-06-15 13:03:23 -0300

    add animation option

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ from src.common.graph import Graph
 parser = argparse.ArgumentParser(description="Run algorithm with input")
 parser.add_argument("algorithm", choices=["edmonds"], help="Algorithm to be runned")
 parser.add_argument("test_id", help="Test id")
+parser.add_argument("-a", "--animate", dest="animate", action="store_true", help="Animate the algorithm")
 args = parser.parse_args()
 
 sys.stdin = open("tests/" + args.test_id, "r")
 
-g = Graph()
-g.read()
+g = Graph(read=True, animate=args.animate)
 
 algo = EdmondsBlossom(g)
 algo.run()
