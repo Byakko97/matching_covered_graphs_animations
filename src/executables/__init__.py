b78f65d3fb4f6e490e5ba57a5d340c0a4a548329
Byakko97 <enriquejh97@gmail.com>
2023-10-14 14:14:08 -0500

    reestructurate project folder

---
+++ src/executables/__init__.py
@@ -0,0 +1,9 @@
+""" Funções executáveis do projeto
+
+Executáveis disponíveis:
+
+- animate:  gera uma animação do algoritmo dado com o teste dado
+- run:      executa sem animação o algoritmo dado com o teste dado
+- test:     testa que o algoritmo esteja bem implementado com todos os testes
+            disponíveis
+"""
